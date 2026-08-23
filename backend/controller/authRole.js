f6bd668fd5fd3f92e02a4f325c21b6c88928129e
eyob <eyobweldiebelay@gmail.com>
2026-08-23 04:29:47 +0300

    authoriztion

---
+++ backend/controller/authRole.js
@@ -0,0 +1,12 @@
+const authorize=(role)=>{
+return(req,res,next)=>{
+if (req.user.role !==role) {
+  return res.status(401).json({msg:"Access denied"});
+    
+}
+next();
+
+}
+
+};
+module.exports=authorize;
